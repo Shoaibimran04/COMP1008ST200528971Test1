c4b8906f986dc423c7ce04463024a50fe393c275
shoaib imran <200528971@student.georgianc.on.ca>
2023-02-02 15:46:50 -0500

    Initial commit

---
+++ src/Clothing.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Clothing {
+    private String Brand;
+    private String Description;
+    private String Colour;
+    public Clothing(String Brand, String Description, String Colour,)
+    {
+        this.setBrand(Brand);
+        this.setDescription(Description);
+        this.setColour(Colour);
+    }
+
